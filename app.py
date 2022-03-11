fbc8a7ac4eb0309bf72e1e1e2645d830a7d297e0
Joshua Huerto <joshuahuerto2@gmail.com>
2022-03-11 15:18:13 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+# Use the package we installed
+from slack_bolt import App
+
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+# Add functionality here
+@app.event("app_mention")
+def mention_handler(body, say):
+  say(body.get("event").get("text"))
+
+
+# Start your app
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
